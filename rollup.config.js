6c4697adfd2bcf8928035b678acf21b59f917f72
HaoliangZhao <haoliang.zhao@ximalaya.com>
2022-06-17 15:32:20 +0800

    feat(renderer): config

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,43 @@
-import typescript from "@rollup/plugin-typescript";
-import css from "rollup-plugin-css-porter"
-export default [{
-    input: "src/stories/components/button/Button.tsx",
-    output: {
-        file: "lib/button/button.js",
-        format: "cjs"
+import typescript from "rollup-plugin-typescript2";
+import css from "rollup-plugin-css-porter";
+import dts from 'rollup-plugin-dts';
+import babel from "@rollup/plugin-babel";
+
+
+const babelOptions = {
+    presets: ["@babel/preset-env"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss'],
+    exclude: "**/node_modules/**"
+}
+const commonPlugins = [
+    typescript(),
+    css(),
+    babel(babelOptions)
+]
+export default [
+    {
+        input: "src/stories/components/index.ts",
+        output: {
+            file: "lib/es/index.js",
+            format: "esm"
+        },
+        plugins: commonPlugins
     },
-    plugins: [typescript(), css()]
-},
-{
-    input: "src/stories/components/header/Header.tsx",
-    output: {
-        file: "lib/header/header.js",
-        format: "cjs"
+    {
+        input: "src/stories/components/index.ts",
+        output: {
+            file: "lib/cjs/index.js",
+            format: "cjs"
+        },
+        plugins: commonPlugins
     },
-    plugins: [typescript(), css()]
-}]
+    {
+        input: "src/stories/components/index.d.ts",
+        output: {
+            file: "lib/types/index.d.ts",
+            format: 'es'
+        },
+        plugins: [...commonPlugins, dts()]
+    }
+
+]
