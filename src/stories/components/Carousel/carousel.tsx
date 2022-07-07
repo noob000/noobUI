5b3b5ed3e5debb3e5ad065201b90fca6a9a04a6d
HaoliangZhao <haoliang.zhao@ximalaya.com>
2022-07-07 16:14:58 +0800

    feat(renderer): carousel component vertical

--- src/stories/components/Carousel/carousel.tsx
+++ src/stories/components/Carousel/carousel.tsx
@@ -6,37 +6,38 @@ import useCarousel from "./useCarousel";
 type CarouselProps = {
     children: ReactElement[];
     autoplay?: boolean;
-    interval?: number
+    interval?: number;
+    dotPosition?: "bottom" | "top" | "left" | "right"
 }
 
-const Carousel: FC<CarouselProps> = ({ children, autoplay = false, interval = 2000 }) => {
+const Carousel: FC<CarouselProps> = ({ children, autoplay = false, interval = 2000, dotPosition = "bottom" }) => {
     const [transition, setTransition] = useState<boolean>(false)
     const [currentIndex, lastIndex, setIndex] = useCarousel(children.length, autoplay, interval)
     const containerRef = useRef<HTMLDivElement | null>(null);
-
+    const isVertical = dotPosition === "left" || dotPosition === "right";
     useEffect(() => {
         if (currentIndex !== 0 && transition) setTransition(false)
     }, [currentIndex])
-   
+
     const translateX = () => {
         if (containerRef.current) {
             if (currentIndex === 0 && !transition && (lastIndex === children.length - 1)) {
                 const container = containerRef.current
-                let left = 0
+                let distance = 0
                 for (let i = 0; i < children.length; i++) {
-                    left += container.children[i]?.clientWidth
+                    distance += isVertical ? container.children[i]?.clientHeight : container.children[i]?.clientWidth
                 }
-                return `translateX(-${left}px)`
+                return isVertical ? `translateY(-${distance}px)` : `translateX(-${distance}px)`
             }
             else if (transition) return 0
-            let left = 0;
+            let distance = 0;
             const container = containerRef.current
             for (let i = 0; i < currentIndex; i++) {
-                left += container.children[i]?.clientWidth
+                distance += isVertical ? container.children[i]?.clientHeight : container.children[i]?.clientWidth
             }
-            return `translateX(-${left}px)`
+            return isVertical ? `translateY(-${distance}px)` : `translateX(-${distance}px)`
         }
-        else return `translateX(0px)`
+        else return isVertical ? `translateY(0px)` : `translateX(0px)`
     }
 
     const handleTransitionEnd = () => {
@@ -56,23 +57,46 @@ const Carousel: FC<CarouselProps> = ({ children, autoplay = false, interval = 20
     }
 
     const containerStyle: CSSProperties = {
+        flexDirection: isVertical ? "column" : "row",
         transform: `${transition ? "translateX(0px)" : translateX()}`,
         transition: `${transition ? "none" : "transform 1s"}`
     }
 
     return (
         <div className="noob-carousel">
-            <div className="noob-carousel-container"
-                onTransitionEnd={handleTransitionEnd}
-                ref={containerRef}
-                style={containerStyle}>
-                {createList()}
-            </div>
-            <Switch
-                size={children.length}
-                callback={(num) => { setIndex(num) }}
-                current={currentIndex}
-                lastIndex={lastIndex} />
+            {isVertical ? <>
+                <Switch
+                    size={children.length}
+                    callback={(num) => { setIndex(num) }}
+                    current={currentIndex}
+                    lastIndex={lastIndex}
+                    dotPosition={dotPosition}
+                    containerRef={containerRef} />
+                <div className="noob-carousel-container"
+                    onTransitionEnd={handleTransitionEnd}
+                    ref={containerRef}
+                    style={containerStyle}>
+                    {createList()}
+                </div>
+            </> :
+                <>
+                    <div className="noob-carousel-container"
+                        onTransitionEnd={handleTransitionEnd}
+                        ref={containerRef}
+                        style={containerStyle}>
+                        {createList()}
+                    </div>
+                    <Switch
+                        size={children.length}
+                        callback={(num) => { setIndex(num) }}
+                        current={currentIndex}
+                        lastIndex={lastIndex}
+                        dotPosition={dotPosition}
+                        containerRef={containerRef} />
+                </>
+            }
+
+
         </div>
     )
 }
--- src/stories/components/Carousel/switch.tsx
+++ src/stories/components/Carousel/switch.tsx
@@ -1,15 +1,19 @@
 import classNames from "classnames";
 import "./style/switch.scss";
-import React, { FC, ReactElement, useEffect, useRef, useState } from "react";
+import React, { CSSProperties, FC, ReactElement, useEffect, useRef, useState } from "react";
 type SwitchProps = {
     lastIndex: number
     size: number;
-    current: number
+    current: number;
+    dotPosition: "bottom" | "top" | "left" | "right"
     callback: (num: number) => void
+    containerRef: React.MutableRefObject<HTMLDivElement | null>
 }
-const Switch: FC<SwitchProps> = ({ size, callback, current, lastIndex }) => {
+const Switch: FC<SwitchProps> = ({ size, callback, current, lastIndex, dotPosition }) => {
+
     const clicked = useRef<boolean>(false)
     const inital = clicked.current || lastIndex !== -1
+    const isVertical = dotPosition === "left" || dotPosition === "right"
     const handleClick = (i: number) => {
         if (!clicked.current) clicked.current = true
         if (i !== current) callback(i)
@@ -18,19 +22,43 @@ const Switch: FC<SwitchProps> = ({ size, callback, current, lastIndex }) => {
     const createElement = () => {
         let elementArray: ReactElement[] = [];
         for (let i = 0; i < size; i++)
-            elementArray.push(
-                <div className={classNames({
-                    "noob-carousel-switch": "true",
-                    "noob-carousel-switch-inital": !inital && i === 0,
-                    "noob-carousel-switch-cur": i === current && inital,
-                    "noob-carousel-switch-last": i === lastIndex
-                })} key={i} onClick={handleClick.bind({}, i)}></div>)
+            if (isVertical) {
+                elementArray.push(
+                    <div className={classNames({
+                        "noob-carousel-switch-v": true,
+                        "noob-carousel-switch-inital-v": !inital && i === 0,
+                        "noob-carousel-switch-cur-v": i === current && inital,
+                        "noob-carousel-switch-last-v": i === lastIndex,
+                    })} key={i} onClick={handleClick.bind({}, i)}></div>)
+            }
+            else {
+                elementArray.push(
+                    <div className={classNames({
+                        "noob-carousel-switch-h": true,
+                        "noob-carousel-switch-inital-h": !inital && i === 0,
+                        "noob-carousel-switch-cur-h": i === current && inital,
+                        "noob-carousel-switch-last-h": i === lastIndex,
+                    })} key={i} onClick={handleClick.bind({}, i)}></div>)
+            }
         return elementArray
     }
+    const basicStyle: CSSProperties = isVertical ?
+        {
+            height: `${(size + 1) * 40}px`,
+            fontSize: "16px"
+        } :
+        {
+            width: `${(size + 1) * 40}px`,
+            fontSize: "16px"
+        }
+    const classStr = `noob-carousel-switch-${dotPosition} noob-carousel-switch-container${isVertical ? "-v" : "-h"}`
     return (
-        <div className="noob-carousel-switch-container" style={{ width: `${(size + 1) * 40}px` }}>
+
+        <div className={classStr}
+            style={basicStyle}>
             {createElement()}
         </div>
+
     )
 }
 export default Switch
