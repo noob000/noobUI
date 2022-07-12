a811144144f2fb41c69cebf973ce65ed7055f5c6
HaoliangZhao <haoliang.zhao@ximalaya.com>
2022-07-12 19:45:51 +0800

    fix(renderer): foldername tolowercase

--- src/stories/components/index.js
+++ src/stories/components/index.js
@@ -1,2 +1,3 @@
 export { default as Message } from "./message"
 export { default as Button } from "./button"
+export { default as Carousel } from "./carousel"
--- src/stories/components/index.ts
+++ src/stories/components/index.ts
@@ -2,4 +2,4 @@ export { default as Button } from "./button";
 export type { ButtonProps } from "./button";
 export { default as Message } from "./message/message";
 export type { MessageApi } from "./message/message"
-export { default as Carousel } from "./Carousel"
+export { default as Carousel } from "./carousel"
